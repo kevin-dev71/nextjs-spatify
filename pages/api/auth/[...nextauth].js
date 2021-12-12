d10cf0309f35b9c28067353104bf3e053dd55ba5
Kevin Dev <kevin.developer71@gmail.com>
2021-12-12 08:18:31 -0600

    NextAuth session with spotify setup

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,31 @@
+// @ts-nocheck
 import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/spotify"
-import { LOGIN_URL } from "../../../lib/spotify"
+import spotifyApi, { LOGIN_URL } from "../../../lib/spotify"
+
+async function refreshAccessToken(token) {
+  try {
+
+    spotifyApi.setAccessToken(token.accessToken)
+    spotifyApi.setRefreshToken(token.refreshToken)
+
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken()
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now() + refreshedToken.expires_in * 1000,
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+
+    }
+
+  } catch (error) {
+
+    console.error(error)
+
+    return { ...token, error: "RefreshAccessTokenError" }
+  }
+}
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -12,4 +37,35 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: '/login'
+  },
+  callbacks: {
+    async jwt({ token, account, user}){
+      // Initiial sign in
+      if(account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000
+        }
+      }
+      // Return previous token if the access token has not expired yet
+      if(Date.now() < token.accessTokenExpires) return token
+
+      // Access token has expired, refresh
+      return await refreshAccessToken()
+    },
+
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken
+      session.user.refreshToken = token.refreshToken
+      session.user.username = token.username
+
+      return session
+    }
+  }
 })
