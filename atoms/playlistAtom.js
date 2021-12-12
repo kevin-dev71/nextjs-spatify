856cf011731843971f8a118190f9f3c8a1b0ac14
Kevin Dev <kevin.developer71@gmail.com>
2021-12-12 17:00:17 -0600

    hook for spotify playlist and atom state

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,12 @@
+// @ts-nocheck
+import { atom } from 'recoil'
+
+export const playlistIdState = atom({
+  key: "playlistIdState",
+  default: ''
+})
+
+export const playlistState = atom({
+  key: "playlistState",
+  default: null,
+})
---
+++ hooks/useSpotify.jsx
@@ -0,0 +1,23 @@
+import { signIn, useSession } from "next-auth/react"
+import { useEffect } from "react"
+import SpotifyWebApi from "spotify-web-api-node"
+import spotifyApi from "../lib/spotify"
+
+function useSpotify() {
+
+  const { data: session, status } = useSession()
+
+  useEffect(() => {
+    if( session ) {
+      if(session.error === 'RefreshAccessTokenError') {
+        signIn()
+      }
+
+      spotifyApi.setAccessToken(session.user.accessToken)
+    }
+  }, [session])
+
+  return spotifyApi
+}
+
+export default useSpotify
