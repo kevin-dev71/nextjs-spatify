f0f0e3a92a50e9ca63c2dd843240b7e636de014d
Kevin Dev <kevin.developer71@gmail.com>
2021-12-12 10:08:08 -0600

    login and auth provider

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,34 @@
-const Login = () => {
+import { getProviders, signIn } from "next-auth/react"
+import Image from "next/image"
+import logoImg from "../public/logo.svg"
+
+const Login = ({ providers }) => {
   return (
-    <div>
-      this is login
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+      <Image src={logoImg} alt="Spotify" className="w-52 mb-5" />
+
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button
+            className="bg-[#18D860] text-white p-5 rounded-full"
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+          >
+            Login with {provider.name}
+          </button>
+        </div>
+      ))}
     </div>
   )
 }
 
 export default Login
+
+export const getServerSideProps = async () => {
+  const providers = await getProviders()
+
+  return {
+    props: {
+      providers,
+    },
+  }
+}
