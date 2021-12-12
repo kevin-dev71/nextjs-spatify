719515b6ca3273b83c8c48af0f1c68a8db3a7436
Kevin Dev <kevin.developer71@gmail.com>
2021-12-12 10:17:42 -0600

    sidebar with hide scrollbar

--- components/Sidebar/index.jsx
+++ components/Sidebar/index.jsx
@@ -6,7 +6,7 @@ function Sidebar() {
   const { data: session, status } = useSession()
 
   return (
-    <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll h-screen">
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen">
       <div className="space-y-4">
         {sideBarBtnsGroup1.map((btn, idx) => (
           <button
